92080170ee0cb66894a5970cde5bab189d8d8411
username <useremail>
2023-12-06 04:29:10 -0800

    week 5 redo submission

--- Python_pizza_app_redo_wk_4.py
+++ Python_pizza_app_redo_wk_4.py
@@ -3,7 +3,7 @@ print("Hi Im  PizzaBot., what do you want?")
 print("Ok  look here im gonna ask you some questions, you answer press enter got it!?  ")
 
 
-userName = input("\nHWhat your name?")
+userName = input(f"\nHWhat your name?")
 if userName.lower() == "Gene Shaffer":
     print(f"\nOh My God!!!!  Its him!!      Its Really him!!    What a pleasure to finally meet you")
 else:
@@ -11,12 +11,24 @@ else:
    
     
 
-size = input("\nWhat size do you want?  Enter    small   medium   or  large:  ")    
+size = input("\nWhat size do you want?  Enter  small,   medium, or  large:  ")
+while len(size) == 0:
+    size = input("Wrong answer  Enter small, medium, or large:  ")
+    
 flavor = input("\nWhat flavor?:  ")
+while len(flavor) == 0:
+    flavor = input("\nYou got to have some flavor, Enter a flavor:  ")
+    
 crustType = input("\nWhat style crust?:  ")
+while len(crustType) == 0:
+    crustType = input("\nWhat kind of crust?:  ")
+    
 quantity = input("\nHow many you want?   Enter a number value:  ")
 quantity = int(quantity)
-method = input("\nIs this carry out or delivery?:  ")
+
+method = input("\nFor carry out or delivery?:  ")
+while method not in ["carry out", "delivery"]:
+    method = input("Not a valid entry! Enter carry out or delivery:  ")
 if method.lower() == "delivery":
     deliveryFee = 5
 else:
